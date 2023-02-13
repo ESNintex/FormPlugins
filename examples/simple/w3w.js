4b8156c1ee44534dbc553d7bfee8b7621ef71a00
ESNintex <122349895+ESNintex@users.noreply.github.com>
2023-02-13 11:04:49 +0200

    Update w3w.js

--- examples/simple/w3w.js
+++ examples/simple/w3w.js
@@ -21,7 +21,7 @@ export class SampleIframe extends LitElement {
     static getMetaConfig() {
         // plugin contract information
         return {
-            controlName: 'W3W Map',
+            controlName: 'W3W-Map',
             fallbackDisableSubmit: false,
             description: 'IFrame component which can render url view with the frame',
             iconUrl: "one-line-text",
