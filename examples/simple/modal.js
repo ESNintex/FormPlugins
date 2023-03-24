af180bc036c8d83d247dc6a4179450dd4140b751
ESNintex <122349895+ESNintex@users.noreply.github.com>
2023-03-24 11:46:28 +0200

    Update modal.js

--- examples/simple/modal.js
+++ examples/simple/modal.js
@@ -1,138 +1,72 @@
-import { LitElement, html, css } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
+import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-const privateOpen = Symbol('open');
-
-class ModalView extends LitElement {
-  static get properties() {
-    return { 
-      open: { type: Boolean, attribute: 'open', reflect: true }
-    };
-  }
-  
-    
-  
-  static get styles() {
-    return [css`.wrapper {
-        opacity: 0;
-        transition: visibility 0s, opacity 0.25s ease-in;
-      }
-      .wrapper:not(.open) {
-        visibility: hidden;
-      }
-      .wrapper.open {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        height: 100vh;
-        position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-        opacity: 1;
-        visibility: visible;
-      }
-      .overlay {
-        background: rgba(0, 0, 0, 0.8);
+export class ModalView extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
+    static styles = css`
+      :host {
         height: 100%;
-        position: fixed;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
         width: 100%;
+        display: block;
       }
-      .dialog {
-        background: #ffffff;
-        max-width: 600px;
-        padding: 1rem;
-        position: fixed;
-      }
-      button {
-        all: unset;
-        cursor: pointer;
-        font-size: 1.25rem;
-        position: absolute;
-          top: 1rem;
-          right: 1rem;
-      }
-      button:focus {
-        border: 2px solid blue;
-      }`];
-  }
-  
-  firstUpdated() {
-    this._watchEscape = this._watchEscape.bind(this);
-  }
-  
-  render() {
-    return html`<button id="launch-dialog">Launch dialog</button>
-    <div class="wrapper ${this.open ? 'open' : ''}" aria-hidden="${!this.open}">
-    <div class="overlay" @click="${this.close}"></div>
-      <div class="dialog" role="dialog" aria-labelledby="title" aria-describedby="content">
-        <button class="close" aria-label="Close" @click=${this.close}>✖️</button>
-        <h1 id="title"><slot name="heading"></slot></h1>
-        <div id="content" class="content">
-          <slot></slot>
-        </div>
-      </div>
-    </div>`;
-  }
 
-  get open() { return this[privateOpen]; }
-  set open(isOpen) {
-    this[privateOpen] = isOpen;
-    const { shadowRoot } = this;
-    const { activeElement } = document;
-    if (isOpen) {
-      setTimeout(() => 
-        shadowRoot.querySelector('button').focus());
-      if (activeElement) {
-        this._wasFocused = activeElement;
+      .frame {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        background-color: transparent;
+        border: none;
       }
-      document.addEventListener('keydown', this._watchEscape);
-    } else {
-      this._wasFocused && this._wasFocused.focus && this._wasFocused.focus();
-      document.removeEventListener('keydown', this._watchEscape);
-    }
-  }
-  
-  close() {
-    this.open = false
-    const closeEvent = new CustomEvent('dialog-closed');
-    this.dispatchEvent(closeEvent);
-  }
-  
-  _watchEscape(event) {
-    if (event.key === 'Escape') {
-        this.close();   
-    }
-  }
-  
-  static getMetaConfig() {
+    `;
+
+    static getMetaConfig() {
         // plugin contract information
         return {
             controlName: 'Modal View',
             fallbackDisableSubmit: false,
-            description: 'Modal frame',
+            description: 'IFrame component which can render url view with the frame',
             iconUrl: "one-line-text",
             groupName: 'Visual',
             version: '1.3',
             properties: {
+                height: {
+                    type: 'string',
+                    title: 'Height',
+                    description: 'Height of the component',
+                    defaultValue: '1000px'
+                },
+                wordAddress: {
+                    type: 'string',
+                    title: 'What3Words address',
+                    description: '3 Word address',
+                    defaultValue: 'pretty.needed.chill'
+                }
             },
             standardProperties: {
                 description: true
             }
         };
     }
+
+    static properties = {
+        height: '1000px',
+        wordAddress: 'pretty.needed.chill'
+    }
+
+    // Render the UI as a function of component state
+    render() {
+        let styles = {height: this.height};
+
+        return html`
+            <iframe
+                    class="frame"
+                    style=${styleMap(styles)}
+                    allow="geolocation *"
+                    src="https://what3words.com/${this.wordAddress}"
+                    height=${this.height}
+            ></iframe>`;
+    }
 }
 
-
+// registering the web component.
 const elementName = 'modal-view';
-customElements.define(elementName, Modal);
-
- /*const button = document.getElementById('launch-dialog');
-button.addEventListener('click', () => {
-  document.querySelector('one-dialog').open = true;
-});*/
-
+customElements.define(elementName, SampleIframe);
